e43e89e25d0674ad1abebe4104a8b2e7c177ed36
NathanMun <nathawut.munsri@gmail.com>
2023-09-15 16:49:28 +0700

    navbar version 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className='container'>
+        <div className='logo'>
+          <p><span className='fit'>Fit</span><span className='hub'>Hub</span></p>          
+        </div>
+        <div className='center__nav'>
+          <a href='#'>Activity</a>
+          <a href='#'>Dashboard</a>
+          <a href='#'>Contact</a>
+        </div>
+        <div className='last__nav'>
+          <a href='#'>Log in</a>
+          <a href='#'>Sign up</a>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar
