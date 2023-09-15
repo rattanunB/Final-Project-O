89ce51bc3bb898ce3d8814ee7929c3193a9736a8
เอกการัณภูมิ ตันเยี่ยนนิติ <thanyalak15601@gmail.com>
2023-09-15 12:03:45 +0700

    activity home page first edit image in css

--- src/pages/ActivitiesPage/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage/ActivitiesPage.jsx
@@ -1,8 +1,85 @@
 import React from 'react'
+import "./ActivitiesPage.css"
 
 const ActivitiesPage = () => {
   return (
-    <div>ActivitiesPage</div>
+    <div className="activity">
+      <div className="activity-type">
+        <div className="activity-image">
+          <img src="https://images.unsplash.com/photo-1571008887538-b36bb32f4571?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="running image" />
+        </div>
+        <div className="activity-detail">
+          <h3>Running</h3>
+          <p>
+          Running is considered a highly beneficial exercise for overall good health. Running is important because it offers a wide range of physical and mental benefits that contribute to a healthier, happier, and more fulfilling life.
+          <br></br>
+          <br></br>
+          It's important to note that while running offers numerous benefits, it's essential to approach it safely and gradually, especially if you're new to running or have underlying health concerns. Consulting with a healthcare professional before beginning a new exercise program is advisable, especially if you have pre-existing health conditions.
+          </p>
+          <button>GET START</button>
+        </div>
+      </div>
+      <div className="activity-type">
+        <div  className="activity-image">
+          <img src="https://i0.wp.com/www.yogabasics.com/yogabasics2017/wp-content/uploads/2021/03/Ashtanga-Yoga.jpeg?w=1080&ssl=1" alt="yoga image"/>
+        </div>
+        <div className="activity-detail">
+          <h3>Yoga</h3>
+          <p>
+          Yoga is a holistic system of physical and mental practices that originated in ancient India. It encompasses a wide range of techniques and philosophies aimed at promoting physical health, mental well-being, and spiritual growth.
+          <br></br>
+          <br></br>
+          Yoga is a versatile practice that can be tailored to an individual's needs and goals. It is important to approach yoga with respect for its tradition and principles while adapting it to one's own personal journey and preferences.
+          </p>
+          <button>GET START</button>
+        </div>
+      </div>
+      <div className="activity-type">
+        <div  className="activity-image">
+          <img src="https://images.unsplash.com/photo-1528629297340-d1d466945dc5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2122&q=80" alt="biking image"/>
+        </div>
+        <div className="activity-detail">
+          <h3>Bike</h3>
+          <p>
+          A "bike," short for "bicycle," is a human-powered, two-wheeled vehicle that is propelled by pedaling. Bicycles have been a popular means of transportation and recreation for many years. 
+          <br></br>
+          <br></br>
+          In summary, biking is important for health because it offers a wide array of physical and mental health benefits, including improved cardiovascular health, muscle strength, mental well-being, and more.
+          </p>
+          <button>GET START</button>
+        </div>
+      </div>
+      <div className="activity-type">
+        <div  className="activity-image">
+          <img src="https://i.pinimg.com/564x/db/a5/6f/dba56f7b5e396df09151ceb9803e0e50.jpg" alt="abs image" />
+        </div>
+        <div className="activity-detail">
+          <h3>ABS</h3>
+          <p>
+          In the context of health and fitness, "ABS" typically refers to abdominal muscles. These are the muscles located in your abdominal region, commonly known as your "core." They play a significant role in various bodily functions and are important for overall health and well-being.
+          <br></br>
+          <br></br>
+          Remember that while having strong abdominal muscles is important, overall fitness and health also depend on factors like a balanced diet, cardiovascular fitness, flexibility, and proper rest. A holistic approach to health and fitness is the key to well-being. 
+          </p>
+          <button>GET START</button>
+        </div>
+      </div>
+      <div className="activity-type">
+        <div  className="activity-image">
+          <img src="https://images.unsplash.com/photo-1517963879433-6ad2b056d712?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="body weight image"/>
+        </div>
+        <div className="activity-detail">
+          <h3>Weight Training</h3>
+          <p>
+          Weight training, also known as strength training or resistance training, is a form of exercise that involves lifting weights or using resistance to increase muscle strength, endurance, and size.
+          <br></br>
+          <br></br>
+          To reap the benefits of weight training while minimizing the risk of injury, it's crucial to use proper form and technique, gradually increase the weight or resistance you use, and allow adequate rest and recovery between workouts.
+          </p>
+          <button>GET START</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
