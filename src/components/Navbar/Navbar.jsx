8783732d5ec130d57dd200db97e23ad61b10fe9e
NathanMun <nathawut.munsri@gmail.com>
2023-09-15 18:10:11 +0700

    add About Us

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <div className='container'>
-        <div className='logo'>
-          <p><span className='fit'>Fit</span><span className='hub'>Hub</span></p>          
+      <div className="container">
+        <div className="logo">
+          <p>
+            <span className="fit">Fit</span>
+            <span className="hub">Hub</span>
+          </p>
         </div>
-        <div className='center__nav'>
-        <a href='#'>Home</a>
-          <a href='#'>Activity</a>
-          <a href='#'>Dashboard</a>
-          <a href='#'>About Us</a>
+        <div className="center__nav">
+          <a href="#">Home</a>
+          <a href="#">Activity</a>
+          <a href="#">Dashboard</a>
+          <a href="#">About Us</a>
         </div>
-        <div className='last__nav'>
-          <a href='#'>Log In</a>
-          <a href='#'>Sign Up</a>
+        <div className="last__nav">
+          <a href="#">Log In</a>
+          <a href="#">Sign Up</a>
         </div>
       </div>
     </>
   );
 };
 
-export default Navbar
+export default Navbar;
