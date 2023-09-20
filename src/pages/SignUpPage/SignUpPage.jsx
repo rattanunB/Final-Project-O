9030ace74a0abff6a0cdcfa3b4508a9ff156d4ff
เอกการัณภูมิ ตันเยี่ยนนิติ <thanyalak15601@gmail.com>
2023-09-20 12:01:11 +0700

    use useState and console.log to see the output when typing in the input field

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,6 +1,30 @@
 import "./SignUpPage.scss";
+import { useState } from 'react';
 
 const SignUpPage = () => {
+
+  const [ firstname, setFirstname ] = useState('');
+  // console.log('Firstnmae:', firstname);
+
+  const [ lastname, setLastname ] = useState('');
+  // console.log('Lastname:',lastname);
+
+  const [ email, setEmail ] = useState('');
+  // console.log('E-mail:',email);
+
+  const [ password, setPassword ] = useState('');
+  // console.log('Password:', password);
+
+  const [ rePassword, setRePassword ] = useState('');
+  // console.log('Re-Password:', rePassword);
+
+  const [ birthdate, setBirthdate ] = useState('');
+  // console.log('Birthdate:', birthdate);
+
+  const [ gender, setGender ] = useState('');
+  // console.log('Gender:', gender);
+
+
   return (
     <div className="signUp-page">
       <div className="signUp-header">
@@ -14,27 +38,27 @@ const SignUpPage = () => {
       <div className="signUp-form">
         <form className="signUp-input">
           <h3>First Name</h3>
-          <input type="text" placeholder="Enter you first name"/>
+          <input type="text" placeholder="Enter you first name" onChange={(e) => setFirstname(e.target.value)} />
           <h3>Last Name</h3>
-          <input type="text" placeholder="Enter you last name"/>
+          <input type="text" placeholder="Enter you last name" onChange={(e) => setLastname(e.target.value)} />
           <h3>E-Mail</h3>
-          <input type="text" placeholder="Enter you e-mail"/>
+          <input type="text" placeholder="Enter you e-mail" onChange={(e) => setEmail(e.target.value)} />
           <h3>Password</h3>
-          <input type="password" placeholder="Enter you password"/>
+          <input type="password" placeholder="Enter you password" onChange={(e) => setPassword(e.target.value)} />
           <h3>Re-Password</h3>
-          <input type="password" placeholder="Enter you password again"/>
+          <input type="password" placeholder="Enter you password again" onChange={(e) => setRePassword(e.target.value)}/>
           <h3>Birthdate</h3>
-          <input type="date"/>
+          <input type="date" onChange={(e) => setBirthdate(e.target.value)} />
           <div className="signUp-gender">
             <h3>Gender</h3>
             <div className="select-gender">
               <div className="gender-male">
-                <input type="radio" value="Male" name="gender" />
-                <label for="Male" >Male</label>
+                <input type="radio" value="Male" name="gender" onChange={(e) => setGender(e.target.value)} />
+                <label htmlFor="Male">Male</label>
               </div>
               <div className="gender-female">
-                <input type="radio" value="Female" name="gender" />
-                <label for="Female">Female</label>
+                <input type="radio" value="Female" name="gender" onChange={(e) => setGender(e.target.value)} />
+                <label htmlFor="Female">Female</label>
               </div>
             </div>
           </div>
