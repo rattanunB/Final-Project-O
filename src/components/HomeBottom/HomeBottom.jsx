9dd478a4de795524ba823eaa4930192a27f5452a
SPCH <spch@SPCH-MacBook.local>
2023-10-09 23:24:50 +0700

    Edit Homebottom1

--- src/components/HomeBottom/HomeBottom.jsx
+++ src/components/HomeBottom/HomeBottom.jsx
@@ -4,23 +4,23 @@ import './HomeBottom.scss';
 const HomeBottom = () => {
   return (
     <div id="home-container">
-        <div class="home-content">
-            <div class="paragraph">
+        <div className="home-content">
+            <div className="paragraph">
                 <h1>GET fit, GET strong.
                     GET healthy</h1>
                 <p>Welcome to our fitness app designed to help you achieve your fitness goal and transform your body and mind.</p>
                 <a href="/activities"><button type="button">GET  START</button></a>
             </div>
-            <div class="activities-pic">
+            <div className="activities-pic">
                 <img src="/HomebottonPic/Bicycle.png" alt='Bicycle' />
             </div>
             
         </div>
-        <div class="home-content">
-            <div class="activities-pic">
+        <div className="home-content">
+            <div className="activities-pic">
                 <img src="/HomebottonPic/Yoka.png" alt='Yoka' />
             </div>
-            <div class="paragraph">
+            <div className="paragraph">
                 <h1>Set Goals. Log Workouts. Stay On Track</h1>
                 <p>Easily track your Workouts, set Training Plans, and discover new Workout Routines to crush your goals.</p>
                 <a href="/creategoal"><button type="button">SET GOAL</button></a>
