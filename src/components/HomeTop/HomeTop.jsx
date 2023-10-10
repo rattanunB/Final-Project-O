8599d265c64748f85b02e92ca7d80f340c6c6847
Pulso <nuchcha.boonyato@gmail.com>
2023-10-10 19:07:11 +0700

    Updated

--- src/components/HomeTop/HomeTop.jsx
+++ src/components/HomeTop/HomeTop.jsx
@@ -9,8 +9,12 @@ const HomeTop = () => {
           <p1>Stay motivated and on track with our powerful exercise tracker</p1> 
             <p3>and access to helpful health content.</p3>
           
-          <button><a href="#">SIGN UP </a></button>
-          <p2>Already member? <u>Log in</u> </p2>
+          <button><a href="/signup">SIGN UP </a></button>
+          <p2>Already member? 
+            <span>
+              <a href="/login">Log In</a>
+            </span>
+            </p2>
         </div>
           <div class="Hometop2">
         </div>
